a21d96ba05fe3de03b1e6b9fc03a0ef075057172
hamid313-coder <hamid.hussainy0@gmail.com>
2023-06-08 06:38:43 +0430

    test commit

--- lib/util/routes/routes.dart
+++ lib/util/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case "a":
       default:
         return MaterialPageRoute(builder: (_) => const Scaffold());
     }
