1ac0be0b1a06a2e487cd7026b341b0eba674274d
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2023-06-08 07:18:35 +0430

    feat(core): add color variables

--- lib/resources/constants/color_block.dart
+++ lib/resources/constants/color_block.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 
 class ColorBlock {
   ColorBlock._();
 
-  static const white = Color(0xffFFFFFF);
-  static const black = Color(0xff000000);
+  static const white = Color(0xffFEFEFE);
+  static const black = Color(0xff333333);
+  static const grey = Colors.grey;
+  static const red = Colors.red;
   static const primary = Color.fromARGB(15, 29, 199, 199);
 }
